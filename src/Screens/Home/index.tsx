e5378ceab24c8d98a61c0743010398df3b6b4e05
Gabriel Cunha <75914436+GahCunha@users.noreply.github.com>
2025-02-23 15:26:05 -0300

    feat: atualizar tela inicial com componentes Header, EventCard e EventGroup

--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,18 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { Container, Title } from './styles';
-import { Image } from 'react-native';
+import { Container } from './styles';
+import Header from 'src/components/Header';
+import EventCard from 'src/components/EventCard';
+import EventGroup from 'src/components/EventGroup';
 
 export default function Home() {
+  const eventCards = [
+    {
+      title: "Assunto",
+      date: "20/02/2025",
+      time: "14:00",
+      iconName: 'info',
+      onPress: () => { }
+    },
+    {
+      title: "Assunto",
+      date: "20/02/2025",
+      time: "14:00",
+      iconName: 'info',
+      onPress: () => { }
+    },
+    {
+      title: "Assunto",
+      date: "20/02/2025",
+      time: "14:00",
+      iconName: 'info',
+      onPress: () => { }
+    },
+    {
+      title: "Assunto",
+      date: "20/02/2025",
+      time: "14:00",
+      iconName: 'info',
+      onPress: () => { }
+    },
+    {
+      title: "Assunto",
+      date: "20/02/2025",
+      time: "14:00",
+      iconName: 'info',
+      onPress: () => { }
+    }
+  ];
   return (
     <>
-    <Container>
-      <Image source={require('@assets/logo.png')} 
-      style={{ width: 200, height: 200 }}
-      />
-      
-      <Title>TELA INICIAL</Title>
-      <StatusBar style="auto" />
-    </Container>
+      <Container>
+        <Header />
+        <EventGroup title="Atual">
+          <EventCard
+            title="Assunto"
+            date="20/02/2025"
+            time="14:00"
+            iconName='edit'
+            onPress={() => { }}
+          />
+        </EventGroup>
+        <EventGroup title="Histórico">
+          {eventCards.map((event, index) => (
+            <EventCard
+              key={index}
+              title={event.title}
+              date={event.date}
+              time={event.time}
+              iconName={event.iconName}
+              onPress={event.onPress}
+            />
+          ))}
+        </EventGroup>
+        <StatusBar style="auto" />
+      </Container>
     </>
   );
 }
--- src/Screens/Home/styles.ts
+++ src/Screens/Home/styles.ts
@@ -1,11 +1,12 @@
 import styled from 'styled-components/native';
 import theme from '../../global/theme';
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
 
 export const Container = styled.View`
     flex: 1;
+    padding: ${getStatusBarHeight() + 20}px 20px 0 20px;
     background-color: ${theme.colors.background};
-    align-items: center;
-    justify-content: center;
     `;
 
 export const Title = styled.Text`
