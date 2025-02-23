09a877ca5004a1e4aca6e6996602fbc9b9a4e20a
João Eduardo <joaojaiba2023@gmail.com>
2025-02-23 18:59:35 -0300

    personalizando bottom tabs

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Home from './src/screens/Home';
 import { Inter_300Light, Inter_400Regular, useFonts } from '@expo-google-fonts/inter';
 import { Roboto_700Bold, Roboto_100Thin } from '@expo-google-fonts/roboto';
 import { Loading } from './src/components/Loading';
+import { Image } from 'react-native';
 
 import TestScreen1 from './src/screens/TestScreen1';
 import TestScreen2 from './src/screens/TestScreen2';
@@ -33,10 +34,49 @@ export default function App() {
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Main">
             {() => (
-              <Tab.Navigator screenOptions={{ headerShown: false }}>
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="TesteScreen1" component={TestScreen1} />
-                <Tab.Screen name="TesteScreen2" component={TestScreen2} />
+              <Tab.Navigator screenOptions={{ headerShown: false,
+                tabBarStyle: {
+                  backgroundColor: '#e6e6e6', 
+                  borderTopWidth: 0, 
+                  elevation: 0, 
+                  shadowOpacity: 0, 
+                  height: 105,
+                },
+                tabBarShowLabel: false,
+                tabBarIconStyle: {
+                  marginTop: 20, 
+                },
+               }}>
+                <Tab.Screen name="Home" component={Home} 
+                options={{
+                  tabBarIcon: ({ focused }) => (
+                    <Image
+                      source={focused ? require('./assets/home2.png') : require('./assets/home.png')}
+                      style={{ width: 30, height: 30 }}
+                      resizeMode="contain"
+                    />
+                  ),
+                }}/>
+                <Tab.Screen name="TesteScreen1" component={TestScreen1} 
+                options={{
+                  tabBarIcon: ({ focused }) => (
+                    <Image
+                      source={focused ? require('./assets/add2.png') : require('./assets/add.png')}
+                      style={{ width: 30, height: 30 }}
+                      resizeMode="contain"
+                    />
+                  ),
+                }}/>
+                <Tab.Screen name="TesteScreen2" component={TestScreen2} 
+                options={{
+                  tabBarIcon: ({ focused }) => (
+                    <Image
+                      source={focused ? require('./assets/user2.png') : require('./assets/user.png')}
+                      style={{ width: 30, height: 30 }}
+                      resizeMode="contain"
+                    />
+                  ),
+                }}/>
               </Tab.Navigator>
             )}
           </Stack.Screen>
